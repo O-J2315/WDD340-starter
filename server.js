7c418437cc20da4c5054396838e881af2a9bdcbd
o-j2315 <J0sue.Hernande2@hotmail.com>
2025-03-04 22:00:45 -0700

    Add initial EJS views and layout structure; remove placeholder files

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
 
 /* ***********************
  * Routes
@@ -27,4 +37,9 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+
+// Index Route
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
 })
