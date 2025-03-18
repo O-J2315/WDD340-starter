875f6e56d939962f6a9dae59604ba4d01b2c2c32
o-j2315 <J0sue.Hernande2@hotmail.com>
2025-03-17 23:09:49 -0600

    Implement base controller for home route and dynamic navigation; add database connection and inventory model

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
+const baseController = require("./controllers/baseController")
+
 
 
 /* ***********************
@@ -40,6 +42,4 @@ app.listen(port, () => {
 })
 
 // Index Route
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" })
-})
+app.get("/", baseController.buildHome)
